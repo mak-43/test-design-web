8a928f81c0a2ac9dc819c07ddfb6c94bfebc6e2e
Mak-43 <letsmailmak@gmail.com>
2022-06-19 03:41:12 +0600

    api destructuring

--- src/Components/Pages/SinglePost.js
+++ src/Components/Pages/SinglePost.js
@@ -1,17 +1,27 @@
 import React from 'react';
 
 const SinglePost = ({post}) => {
+    const {created_at,post_details,liked_posts_count,comments_count}=post
     const {first_name,last_name,username}=post.user
+    const {content_name}=post.post_contents
     const {friend_profile_photo}=post.profile_images
 
     return (
         <div className='bg-base-200 mx-10 rounded'>
                  <div class="card w-full  m-5">
                     <div class="card-body ">
+                        <img src={friend_profile_photo} alt="" />
                         <h2 class="card-title">{first_name +" "+last_name}</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div class="card-actions ">
-                           hello
+                        <small>{created_at}</small>
+                        <div>
+                            <img src={content_name} alt="" />
+                            
+                        </div>
+                        <p>{post_details}</p>
+                        <div>
+                            <p>{liked_posts_count} Like</p>
+                            <p>{comments_count} Comment</p>
+                            <p>Share</p>
                         </div>
 
                     </div>
