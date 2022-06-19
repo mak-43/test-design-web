e88685789312d83e55110f66e5d17e9b177758c4
Mak-43 <letsmailmak@gmail.com>
2022-06-19 11:17:13 +0600

    pagination array

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,16 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import SinglePost from './SinglePost';
-
+import './Home.css'
 const Home = () => {
     const [posts, setPost] = useState([])
     const [pageCount,setPageCount]=useState(0)
-
-    const pages=Math.ceil(posts.length/2)
-  
+    console.log(pageCount)
+    
+    
     useEffect(() => {
         fetch(`https://backend.uviom.com/frontend_api/test-data`)
             .then(res => res.json())
-            .then(data => setPost(data.data))
+            .then(data => {setPost(data.data)
+            
+                const count=data.data.length
+                const pages=Math.ceil(count/2)
+                console.log(count)
+                setPageCount(pages)
+            })
     }, [])
     return (
         <div className='lg:px-20  lg:w-2/3 mx-auto'>
@@ -22,8 +28,13 @@ const Home = () => {
                     post={p}
                     ></SinglePost>)
                 }
+            
 
-
+            </div>
+            <div className='pagination text-center my-5'>
+            {
+                [...Array(pageCount).keys()].map(number=><button className='px-2'>{number+1}</button>)
+            }
             </div>
         </div>
     );
