49a8c589642abb7cd76def728aaba725055b5a6a
Mak-43 <letsmailmak@gmail.com>
2022-06-19 10:02:12 +0600

    like comment

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -3,14 +3,18 @@ import SinglePost from './SinglePost';
 
 const Home = () => {
     const [posts, setPost] = useState([])
+    const [pageCount,setPageCount]=useState(0)
+
+    const pages=Math.ceil(posts.length/2)
+  
     useEffect(() => {
         fetch(`https://backend.uviom.com/frontend_api/test-data`)
             .then(res => res.json())
             .then(data => setPost(data.data))
     }, [])
     return (
-        <div>
-            Total Post : {posts.length}
+        <div className='lg:px-20  lg:w-2/3 mx-auto'>
+            Total Posts : {posts.length}
             <div className='bg-base-100'>
                 {
                     posts.map(p =><SinglePost
--- src/Components/Pages/SinglePost.js
+++ src/Components/Pages/SinglePost.js
@@ -1,31 +1,38 @@
 import React from 'react';
 
-const SinglePost = ({post}) => {
-    const {created_at,post_details,liked_posts_count,comments_count}=post
-    const {first_name,last_name,username}=post.user
-    const {content_name}=post.post_contents
-    const {friend_profile_photo}=post.profile_images
+const SinglePost = ({ post }) => {
+    const { created_at, post_details, liked_posts_count, comments_count } = post
+    const { first_name, last_name, username } = post.user
+    const { content_name } = post.post_contents
+    const { friend_profile_photo } = post.profile_images
 
     return (
-        <div className='bg-base-200 mx-10 rounded'>
-                 <div class="card w-full  m-5">
-                    <div class="card-body ">
-                        <img src={friend_profile_photo} alt="" />
-                        <h2 class="card-title">{first_name +" "+last_name}</h2>
-                        <small>{created_at}</small>
+        <div className='bg-base-200  rounded'>
+            <div class="card w-full  m-5">
+                <div class="card-body ">
+                    <div className='flex justify-between items-center'>
                         <div>
-                            <img src={content_name} alt="" />
-                            
+                            <img src={friend_profile_photo} alt="" />
+                            <h2 class="card-title">{first_name + " " + last_name}</h2>
+                            <small>{created_at}</small>
                         </div>
-                        <p>{post_details}</p>
-                        <div className='flex  items-center gap-5 '>
-                            <p className='bg-base-100 rounded-full p-3  flex justify-center items-center gap-2 flex-wrap'><i class="fa-solid fa-thumbs-up"></i> {liked_posts_count} Like</p>
-                            <p className='bg-base-100 rounded-full p-3  flex justify-center items-center gap-2'><i class="fa-solid fa-comments"></i> {comments_count} Comment</p>
-                            <p className='bg-base-100 rounded-full p-3  flex justify-center items-center gap-2'> <i class="fa-solid fa-share-from-square"></i>Share</p>
+                        <div>
+                            <i class="fa-solid fa-ellipsis m-5"></i>
                         </div>
+                    </div>
+                    <div>
+                        <img src={content_name} alt="" />
 
                     </div>
+                    <p>{post_details}</p>
+                    <div className='flex  items-center justify-center gap-5 '>
+                        <p className='bg-base-100 rounded-full p-2 w-1 flex justify-center items-center gap-2 flex-wrap'><i class="fa-solid fa-thumbs-up"></i> {liked_posts_count} Like</p>
+                        <p className='bg-base-100 rounded-full p-2 w-1 flex justify-center items-center gap-2 flex-wrap'><i class="fa-solid fa-comments"></i> {comments_count} Comment</p>
+                        <p className='bg-base-100 rounded-full p-2 w-1 flex justify-center items-center gap-2 flex-wrap'> <i class="fa-solid fa-share-from-square"></i>Share</p>
+                    </div>
+
                 </div>
+            </div>
         </div>
     );
 };
