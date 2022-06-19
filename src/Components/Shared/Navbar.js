df880ba84537b59919d4e1580aeffad5c6a3fc4b
Mak-43 <letsmailmak@gmail.com>
2022-06-19 20:09:52 +0600

    contact form

--- src/Components/Shared/Navbar.js
+++ src/Components/Shared/Navbar.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link,NavLink } from "react-router-dom";
 
 const Navbar = () => {
     return (
         <div className="my-5 flex justify-evenly items-center">
-            <Link to='/'>Home</Link>
-            <Link to='/contact'>Contact</Link>
+            <NavLink className='rounded-lg bg-base-300 p-2 hover:bg-base-100 border' to='/'>Home</NavLink>
+            <NavLink className='rounded-lg bg-base-300 p-2 hover:bg-base-100 border' to='/contact'>Contact</NavLink>
         </div>
     );
 };
